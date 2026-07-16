ce6955eb8be80855e0e4b4610bbd47ed4ded9ecc
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-16 14:47:18 +0200

    Add funcload.c for function loading functionality

---
+++ dpp/funcload.c
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <stdint.h>
+
+uint64_t nload = reintepeter_cast<void*> loadfunc(90);
+
+enum tokeload
+{
+    NEURAL,
+    SYSTEM
+};
+
+DOWRD functionload = static_cast<uint64*> nload;
+
